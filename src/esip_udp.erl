1dd0c96d334c4bded766f19e13e88098bf7406ad
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2011-03-15 18:09:39 +0900

    Implement multiple UDP dispatchers

--- src/esip_udp.erl
+++ src/esip_udp.erl
@@ -19,7 +19,7 @@
 -include("esip.hrl").
 -include("esip_lib.hrl").
 
--record(state, {sock, ip, port}).
+-record(state, {sock, ip, port, workers = []}).
 
 %%====================================================================
 %% API
@@ -30,6 +30,9 @@ start_link(Port, Opts) ->
 start(Port, Opts) ->
     supervisor:start_child(esip_udp_sup, [Port, Opts]).
 
+start(State) ->
+    gen_server:start(?MODULE, [State], []).
+
 send(Sock, {Addr, Port}, Data) ->
     gen_udp:send(Sock, Addr, Port, Data).
 
@@ -39,6 +42,8 @@ connect([AddrPort|_Addrs], Sock) ->
 %%====================================================================
 %% gen_server callbacks
 %%====================================================================
+init([State]) ->
+    {ok, State};
 init([Port, Opts]) ->
     case gen_udp:open(Port, [binary,
 			     {active, once} | Opts]) of
@@ -46,9 +51,9 @@ init([Port, Opts]) ->
             case inet:sockname(S) of
                 {ok, {IP, _}} ->
                     State = #state{sock = S, ip = IP, port = Port},
-                    esip_transport:register_udp_listener(
-                      make_socket(State)),
-                    {ok, State};
+                    Workers = start_workers(State),
+                    esip_transport:register_udp_listener(make_socket(State)),
+                    {ok, State#state{workers = Workers}};
                 {error, Reason} ->
                     {stop, Reason}
             end;
@@ -66,10 +71,35 @@ handle_info({udp, S, IP, Port, Data}, #state{sock = S} = State) ->
     MyAddr = {State#state.ip, State#state.port},
     SIPSock = #sip_socket{type = udp, sock = S,
                           addr = MyAddr, peer = {IP, Port}},
+    inet:setopts(S, [{active, once}]),
+    case State#state.workers of
+        [] ->
+            MyAddr = {State#state.ip, State#state.port},
+            SIPSock = #sip_socket{type = udp, sock = S,
+                                  addr = MyAddr, peer = {IP, Port}},
+            esip:callback(data_in, [Data, SIPSock]),
+            transport_recv(SIPSock, Data),
+            {noreply, State};
+        [Pid|Pids] ->
+            Pid ! {self(), IP, Port, Data},
+            {noreply, State#state{workers = Pids}}
+    end;
+handle_info({Parent, IP, Port, Data}, #state{sock = S} = State) ->
+    MyAddr = {State#state.ip, State#state.port},
+    SIPSock = #sip_socket{type = udp, sock = S,
+                          addr = MyAddr, peer = {IP, Port}},
     esip:callback(data_in, [Data, SIPSock]),
     transport_recv(SIPSock, Data),
-    inet:setopts(S, [{active, once}]),
+    Parent ! {worker_done, self()},
     {noreply, State};
+handle_info({'DOWN', _MonitorRef, _Type, Pid, Info}, State) ->
+    ?ERROR_MSG("restarting UDP worker, "
+               "because it has died with reason: ~p", [Info]),
+    Worker = start_worker(State),
+    NewWorkers = Worker ++ lists:delete(Pid, State#state.workers),
+    {noreply, State#state{workers = NewWorkers}};
+handle_info({worker_done, Pid}, #state{workers = Pids} = State) ->
+    {noreply, State#state{workers = [Pid|Pids]}};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -117,3 +147,24 @@ do_transport_recv(SIPSock, Msg) ->
 
 make_socket(#state{sock = S, ip = IP, port = Port}) ->
     #sip_socket{type = udp, sock = S, addr = {IP, Port}}.
+
+start_workers(State) ->
+    case erlang:system_info(logical_processors) of
+        N when N > 1 ->
+            lists:flatmap(
+              fun(_) ->
+                      start_worker(State)
+              end, lists:seq(1, N));
+        _ ->
+            []
+    end.
+
+start_worker(State) ->
+    case start(State#state{workers = []}) of
+        {ok, Pid} ->
+            erlang:monitor(process, Pid),
+            [Pid];
+        Err ->
+            ?ERROR_MSG("failed to start UDP worker: ~p", [Err]),
+            []
+    end.
