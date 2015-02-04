299225e80ad40ecb81666cde3b21a05893be7c97
Christophe Romain <christophe.romain@process-one.net>
2015-02-04 13:39:27 +0100

    Update copyright up to 2015

--- src/esip_udp_sup.erl
+++ src/esip_udp_sup.erl
@@ -1,11 +1,29 @@
-%%%-------------------------------------------------------------------
-%%% @author Evgeny Khramtsov <ekhramtsov@process-one.net>
-%%% @copyright (C) 2014, Evgeny Khramtsov
-%%% @doc
+%%%----------------------------------------------------------------------
+%%% File    : esip_udp_sup.erl
+%%% Author  : Evgeniy Khramtsov <ekhramtsov@process-one.net>
+%%% Purpose : 
+%%% Created : 25 Apr 2014 by Evgeniy Khramtsov <ekhramtsov@process-one.net>
 %%%
-%%% @end
-%%% Created : 25 Apr 2014 by Evgeny Khramtsov <ekhramtsov@process-one.net>
-%%%-------------------------------------------------------------------
+%%%
+%%% p1_sip, Copyright (C) 2002-2015   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
+%%%----------------------------------------------------------------------
+
 -module(esip_udp_sup).
 
 -behaviour(supervisor).
